c0ea6500e6a86f5e987f14c0ad619a8bc71b89c7
yves makwambala <yvesmak1@gmail.com>
2021-02-14 00:58:43 +0100

    Ma premier version d'angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NomPremierComponent } from './nom-premier/nom-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NomPremierComponent,
+    AppareilComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName;
+  @Input() appareilStatus;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getStatus(){
+    return this.appareilStatus;
+  }
+  getColor(){
+    if(this.appareilStatus === 'allumé'){
+      return 'green';
+    } else if(this.appareilStatus === 'éteint'){
+      return 'red';
+    }
+  }
+
+}
