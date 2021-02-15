fa192dc77116e46e2367621a49ebbb1d83d99bd1
yves makwambala <yvesmak1@gmail.com>
2021-02-15 17:20:59 +0100

    modification du lundi 15/02/2021

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil',
@@ -9,15 +10,26 @@ export class AppareilComponent implements OnInit {
 
   @Input() appareilName;
   @Input() appareilStatus;
+  @Input() index;
 
-  constructor() { }
+  constructor(private appareilService: AppareilService) {}
 
   ngOnInit(): void {
+
+  }
+
+  onSwitch(){
+    if(this.appareilStatus === 'allumé'){
+      this.appareilService.switchOffOne(this.index);
+    } else if(this.appareilStatus === 'éteint'){
+      this.appareilService.switchOnOne(this.index);
+    }
   }
 
   getStatus(){
     return this.appareilStatus;
   }
+
   getColor(){
     if(this.appareilStatus === 'allumé'){
       return 'green';
